5c356bd0787f0dd688af0059e1e9fd90259c4dbe
harris-chris <chrisharriscjh@gmail.com>
2021-03-10 20:24:02 +0900

    Update Mill to 0.9.5 in shell.nix

--- shell.nix
+++ shell.nix
@@ -2,9 +2,9 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
   inherit (pkgs.lib) optional optionals;
-  mill = import sources.mill;
+  #mill = import sources.mill;
 in pkgs.mkShell {
-  buildInputs = [ #mill 
+  buildInputs = [ pkgs.mill
   ];
   MILL_VERSION = "0.9.5";
 }
