269146127550a07dc3a9896659cdb8589af9d9d5
harris-chris <chrisharriscjh@gmail.com>
2021-03-12 08:24:09 +0900

    Fix main function in scala3 for scala2/3 cross build

--- Scala3Scala2CrossBuild-Mill-Utest/HelloWorld/3.0.0-RC1/main/scala/ScalaDate/HelloWorld.scala
+++ Scala3Scala2CrossBuild-Mill-Utest/HelloWorld/3.0.0-RC1/main/scala/ScalaDate/HelloWorld.scala
@@ -1,7 +1,8 @@
 package helloworld
 
+@main def checkMain = println("VERSION 3.0.0-RC1 running")
+
 object HelloWorld {
-  def main(args: Array[String]) = println("VERSION 3.0.0-RC1 running")
   val versionCheck = "3.0.0-RC1"
 }
 
